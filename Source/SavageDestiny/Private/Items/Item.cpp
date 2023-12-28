56d030b7b226c052e5bb0398ae7c140d9a87b254
Franklin Ader Mba Wetbe <adereinstein1@gmail.com>
2023-12-27 16:39:17 -0800

    Add DebugMacros.h

--- Source/SavageDestiny/Private/Items/Item.cpp
+++ Source/SavageDestiny/Private/Items/Item.cpp
@@ -1,6 +1,7 @@
 // Copyright: Ader Einstein 2023
 
 #include "Items/Item.h"
+#include "SavageDestiny/DebugMacros.h"
 
 AItem::AItem()
 {
@@ -10,6 +11,12 @@ AItem::AItem()
 void AItem::BeginPlay()
 {
 	Super::BeginPlay();
+
+	FVector Location = GetActorLocation();
+	FVector Forward = GetActorForwardVector();
+
+	DRAW_SPHERE(Location);
+	DRAW_VECTOR(Location, Location + Forward * 100.f);
 }
 
 void AItem::Tick(float DeltaTime)
